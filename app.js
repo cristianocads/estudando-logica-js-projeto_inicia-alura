f362c164a6312ba261a80ca9d32fbbeb3b826e6c
Cristiano Aguiar <162627463+cristianocads@users.noreply.github.com>
2024-03-13 15:07:52 -0300

    ajuste

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("Vai estudar cara!");
+alert("Jogo qual é o número");
+let numeroSecreto = 29;
+let chute = prompt("Escolha um número entre 1 e 30");
+
+if (chute == numeroSecreto) {
+    console.log("Isso aí! Você acertou o número (29)");
+}
