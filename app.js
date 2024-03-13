f8c642dab0a18aed2019817a316a909e5954dfd9
Cristiano Aguiar <162627463+cristianocads@users.noreply.github.com>
2024-03-13 17:02:27 -0300

    continuação

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 alert("Jogo qual é o número");
 let numeroSecreto = 29;
+console.log(numeroSecreto)
 let chute = prompt("Escolha um número entre 1 e 30");
 
 if (chute == numeroSecreto) {
-    console.log("Isso aí! Você acertou o número (29)");
+    alert(`Isso aí! Você acertou o número ${numeroSecreto}`);
+} else {
+    alert("Você errou:(")
 }
