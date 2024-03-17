549db16c54631d2ac1fb1f220f3c3681d27d42e0
Cristiano Aguiar <162627463+cristianocads@users.noreply.github.com>
2024-03-16 22:10:41 -0300

    ajuste

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 alert("Jogo qual é o número");
 let numeroSecreto = 29;
-console.log(numeroSecreto)
-let chute = prompt("Escolha um número entre 1 e 30");
+console.log(numeroSecreto);
+let chute ;
 console.log('Valor do chute:', chute);
 
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você acertou o número ${numeroSecreto}`);
-} else {
-    alert("Você errou:(")
+
+// Enquanto
+// != (não for igual, diferente)
+// ==  (uma variável de valor igual a outro)
+while (chute != numeroSecreto){
+    chute = prompt("Escolha um número entre 1 e 30");
+
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você acertou o número ${numeroSecreto}`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número é maior que ${chute}`);
+        }
+        alert("Você errou:(")
+    }
 }
 
 
-
+// Exercício 2
 // Adicionando uma condicional para dia de semana.
 let dia = prompt("qual o dia da semana?");
 
